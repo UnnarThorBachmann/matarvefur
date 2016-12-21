3568c5a6ee6a37ec97f511903f4e1c553d7a47b6
Unnar Þór Bachmann <uthb@hotmail.com>
2016-12-21 17:08:14 +0000

    fix: Sorting the food item list and styling the input.

--- label-setup.py
+++ label-setup.py
@@ -3,9 +3,15 @@ import codecs
 
 f = codecs.open('isgem2.csv', encoding='ISO-8859-1')
 f2 = codecs.open('flokkar.txt','w')
+heitin = []
 for line in f:
     items = line.split(";")
     heiti = items[1]
+    heitin.append(heiti)
+
+heitin = sorted(heitin)
+for heiti in heitin:
     f2.write(('<option value="%s"> </option>\n' % heiti).encode('utf-8'))
+
 f2.close()
 f.close()

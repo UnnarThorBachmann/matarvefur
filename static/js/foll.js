2609a5dbfcdac80646e44ebc5da845975075d933
Unnar Þór Bachmann <uthb@hotmail.com>
2016-12-22 22:43:13 +0000

    feat: Adding collapses (bootstrap) for each food category. Displaying each food_item as thumbnail (bootstrap).

--- static/js/foll.js
+++ static/js/foll.js
@@ -1,3 +1,14 @@
+var kommufall = function(t) {
+  if (typeof t == "undefined") {
+   return '<óskilgreint>';
+ }
+ else if (isNaN(t)){
+ 	return '<óskilgreint>';
+ }
+ else {
+    return (t.toString()).replace('.',',');
+ }
+};
 var google = google || {};
 
 google.appengine = google.appengine || {};
@@ -12,12 +23,25 @@ google.appengine.matarapp.enableButton = function() {
 	valtakki.addEventListener('click', function(e) {
 		var matur = document.getElementById("matarlistiInput");
 		gapi.client.matarvefur.food_item_get({'food_item_heiti': matur.value}).execute(function(resp) {
-			var node = document.getElementById("food_item");
+			var node = document.getElementById("results");
 			while (node.firstChild) {
     			node.removeChild(node.firstChild);
 			}
 			if (!resp.code) {
-				console.log(resp);
+				var template = _.template(naertafla);
+    			document.getElementById("results").innerHTML = template({
+              			heiti: resp['heiti'],
+              			hitaeiningar: kommufall((37*resp['fita'] + 17*resp['protein'] + 17*resp['kolvetni_alls']).toFixed(0)),
+              			fita: kommufall(resp['fita']),
+              			protein: kommufall(resp['protein']),
+              			kolesterol: kommufall(resp['kolestrol']),
+              			kolvetni: kommufall(resp['kolvetni_alls']),
+              			a_vitamin_rj: kommufall(resp['a_vitamin_rj']),
+              			c_vitamin: kommufall(resp['c_vitamin']),
+              			kalk: kommufall(resp['kalk']),
+              			jarn: kommufall(resp['jarn'])
+    			}); 
+				/*
 				var food_item_div = document.getElementById("food_item");
 				var naering_listi = document.createElement('ul');
 				naering_listi.setAttribute('class','list-group');
@@ -40,17 +64,17 @@ google.appengine.matarapp.enableButton = function() {
 					naering_listi.append(naering_item);
 					food_item_div.append(naering_listi);
 					}
-				}
+				}*/
 			}
 			else {
-				var food_item_div = document.getElementById("food_item");
-				var naering_listi = document.createElement('ul');
-				naering_listi.setAttribute('class','list-group');
-				var naering_item = document.createElement('li');
-				naering_item.setAttribute('class','list-group-item');
-				naering_item.innerHTML ='<b>Fannst ekki í gagnagrunni</b>';
-				naering_listi.append(naering_item);
-				food_item_div.append(naering_listi);
+				var res = document.getElementById("results");
+				var div_thumb = document.createElement('div');
+				div_thumb.setAttribute('class','thumbnail');
+				var div = document.createElement('div');
+				div.setAttribute('class','caption');
+				div.innerHTML='<h4><strong>Fannst ekki</strong></h4>';
+				div_thumb.append(div);
+				res.append(div_thumb);
 			}
 		});
 	});
@@ -66,3 +90,13 @@ google.appengine.matarapp.init = function(apiRoot) {
 	apisToLoad = 1;
 	gapi.client.load('matarvefur', 'v1', callback, apiRoot);
 };
+
+/*
+var flokkar = document.getElementsByClass('flokkar');
+for (flokkur in flokkar) {
+	flokkur.addEventListener('click', function (e) {
+	  return function () {
+		$(document).scrollTop("500");
+	  }
+	});
+}*/
---
+++ static/js/thumb.js
@@ -0,0 +1,31 @@
+var naertafla= '<div class="thumbnail"><div class="caption"><h4><strong><%=heiti%></strong>\
+</h4><h5>Næringargildi í 100 g:</h5>\
+<table class="table-responsive">\
+<tr>\
+<th>Orka</th>\
+<th></th>\
+<th>vítamín og steinefni</th>\
+</tr>\
+<tr>\
+<td>Hitaeiningar: <%=hitaeiningar%> kJ</td>\
+<td></td>\
+<td>A-vítamín: <%=a_vitamin_rj%> míkró grömm</td>\
+</tr>\
+<tr>\
+<td>Protein: <%=protein%> g</td>\
+<td></td>\
+<td>C-vítamín: <%=c_vitamin%> mg</td>\
+</tr>\
+<tr>\
+<td>Kolvetni: <%=kolvetni%> g</td>\
+<td></td>\
+<td>Kalk: <%=kalk%> mg</td>\
+</tr>\
+<tr>\
+<td>Kólesteról: <%=kolesterol%> mg</td>\
+<td></td>\
+<td>Járn: <%=jarn%> mg</td>\
+</tr>\
+</table>\
+</div><button type="button" class="btn btn-primary databasebtn" id="skoda">Nánar</button></div>\
+</table>';
